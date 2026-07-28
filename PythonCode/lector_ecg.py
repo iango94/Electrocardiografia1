d5056b5eb526c0705d32a7cf2502e5278273b4e1
ian <iango_93@hotmail.com>
2026-07-28 15:41:09 -0500

    error1

--- PythonCode/lector_ecg.py
+++ PythonCode/lector_ecg.py
@@ -1,6 +1,7 @@
 import board
 import busio
 import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
 
 class LectorECG:
     def __init__(self):
@@ -9,41 +10,38 @@ class LectorECG:
         
         # Inicializar ambos ADS1115
         self.ads_1 = ADS.ADS1115(self.i2c, address=0x48)
-        #self.ads_2 = ADS.ADS1115(self.i2c, address=0x49)
+        # self.ads_2 = ADS.ADS1115(self.i2c, address=0x49)
         
-        # Modo continuo y velocidad máxima de 860 SPS
-        self.ads_1.mode = 0  
-        #self.ads_2.mode = 0  
+        # Configurar tasa de muestreo (860 SPS) y ganancia (1 = +/- 4.096V)
         self.ads_1.data_rate = 860  
-        #self.ads_2.data_rate = 860  
-        
-        # Ganancia fija (Modificar según la salida de tu front-end)
         self.ads_1.gain = 1
-        #self.ads_2.gain = 1
         
-        # Constantes de multiplexación diferencial
-        self.MUX_DI  = 0b100  # AIN0 vs AIN1
-        self.MUX_DII = 0b101  # AIN2 vs AIN3
-        #self.MUX_V1  = 0b100  # AIN0 vs AIN1
-        #self.MUX_V5  = 0b101  # AIN2 vs AIN3
+        # self.ads_2.data_rate = 860  
+        # self.ads_2.gain = 1
+        
+        # Definir canales diferenciales
+        # P0_P1 equivale a MUX 0b100 (AIN0 vs AIN1)
+        # P2_P3 equivale a MUX 0b101 (AIN2 vs AIN3)
+        self.chan_di = AnalogIn(self.ads_1, ADS.P0, ADS.P1)
+        self.chan_dii = AnalogIn(self.ads_1, ADS.P2, ADS.P3)
+        
+        # self.chan_v1 = AnalogIn(self.ads_2, ADS.P0, ADS.P1)
+        # self.chan_v5 = AnalogIn(self.ads_2, ADS.P2, ADS.P3)
         
         # Factor de conversión (Bits a Voltios)
         self.VOLTS_PER_BIT = 4.096 / 32767
 
     def leer_canales(self):
-        """Lee de inmediato el registro de hardware de ambos chips"""
-        # Lectura ADS 1
-        self.ads_1.mux = self.MUX_DI
-        v_di = self.ads_1.get_last_reading() * self.VOLTS_PER_BIT
+        """Lee la tensión en voltios de los canales configurados."""
+        # Opción A: Usar directamente .voltage (calcula los voltios en base a la ganancia automáticamente)
+        v_di = self.chan_di.voltage
+        v_dii = self.chan_dii.voltage
         
-        self.ads_1.mux = self.MUX_DII
-        v_dii = self.ads_1.get_last_reading() * self.VOLTS_PER_BIT
+        # Opción B: Si prefieres aplicar tu constante manual con .value (bits crudos):
+        # v_di = self.chan_di.value * self.VOLTS_PER_BIT
+        # v_dii = self.chan_dii.value * self.VOLTS_PER_BIT
         
-        # Lectura ADS 2
-        #self.ads_2.mux = self.MUX_V1
-        #v_v1 = self.ads_2.get_last_reading() * self.VOLTS_PER_BIT
-        
-        #self.ads_2.mux = self.MUX_V5
-        #v_v5 = self.ads_2.get_last_reading() * self.VOLTS_PER_BIT
+        # v_v1 = self.chan_v1.voltage
+        # v_v5 = self.chan_v5.voltage
         
         return v_di, v_dii #, v_v1, v_v5
